1a1604daf696aa4f9a54b97a66f0e0f4fd14cfb3
fprog2021 <92251492+MRfrog1111@users.noreply.github.com>
2023-11-22 12:46:38 +0700

    Фикс прыжка
    Добавлено замедление через drag и изменена сила 2 прыжка

--- PGC Game 2024/Assets/Scripts/MovingController.cs
+++ PGC Game 2024/Assets/Scripts/MovingController.cs
@@ -1,53 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovingController : MonoBehaviour
 {
+    [Header("Preferences")]
     public float movingSpeed;
     public float jumpForce;
     public float doubleJumpForce;
-    private Vector3 movingVector;
-
+    
+    [Header("Instances")]
+    public LayerMask Ground;
     private Rigidbody mc_rb;
+
+    [Header("Techincal Variables")]
+    private Vector3 movingVector;
     private bool canDoubleJump = false;
-    private bool canJump = true;
-    // Start is called before the first frame update
+    private bool canJump ;
     void Start()
     {
         mc_rb = gameObject.GetComponent<Rigidbody>();
     }
 
-
-    // Update is called once per frame
     void Update()
+    {
+
+        canJump = Physics.Raycast(transform.position, Vector3.down, 1.2f, Ground);
+        
+        Move();
+
+       if (Input.GetKeyDown(KeyCode.Space))
+       {
+
+        Jump();
+
+       }
+
+    }
+
+    //Ñîçäà¸ì äâèæåíèÿ ïåðñîíàæà ïî ãîðèçîíòàëè è âåðòèêàëè
+    private void Move() 
     {
         
         movingVector.x = Input.GetAxis("Horizontal");
         movingVector.z = Input.GetAxis("Vertical");
+
         mc_rb.MovePosition(mc_rb.position + movingVector * (movingSpeed * Time.deltaTime));
-       if (Input.GetKeyDown(KeyCode.Space))
-       {
-           if (canJump)
-           {
-               mc_rb.velocity = new Vector3(mc_rb.velocity.x, jumpForce, mc_rb.velocity.z);
-               canJump = false;
-               canDoubleJump = true;
-           }
-           else if (canDoubleJump)
-           {
-               mc_rb.velocity = new Vector3(mc_rb.velocity.x, doubleJumpForce, mc_rb.velocity.z);
-               canDoubleJump = false;
-           }
-       }
+
     }
 
-    void OnCollisionEnter(Collision coll)
+    //Ïîçâîëÿåì åìó ïðûãàòü
+    private void Jump() 
     {
-        if (coll.collider.CompareTag("Ground"))
+        if (canJump) 
         {
-            canJump = true;
+                   mc_rb.velocity = new Vector3(mc_rb.velocity.x, jumpForce, mc_rb.velocity.z);
+                   canJump = false;
+                   canDoubleJump = true;
+        }
+        else if (canDoubleJump)
+        {
+                   mc_rb.velocity = new Vector3(mc_rb.velocity.x, doubleJumpForce, mc_rb.velocity.z);
+                   canDoubleJump = false;
         }
     }
 }
+
+
