8565c6d8bcd7cff822299179a3f233760afaa305
Tapok <92251492+MRfrog1111@users.noreply.github.com>
2023-12-10 15:59:37 +0700

    Загрузка сохраненных параметров

--- PGC Game 2024/Assets/Scripts/MC_InGameInformation.cs
+++ PGC Game 2024/Assets/Scripts/MC_InGameInformation.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MC_InGameInformation : MonoBehaviour
 {
     public int hp;
@@ -10,17 +11,29 @@ public class MC_InGameInformation : MonoBehaviour
     public int collectedHoney;
     public int maxHoneyAmount;
     public int swordAura;
-    public int LevelNum;
     public List<string> collectibles;
     public List<string> shopList;
+    void Awake(){
+        if (PlayerPrefs.HasKey("posX"))
+        {
+            gameObject.transform.position = new Vector3(PlayerPrefs.GetFloat("posX"),PlayerPrefs.GetFloat("posY"),PlayerPrefs.GetFloat("posZ"));
+            maxHp = PlayerPrefs.GetInt("HealthPoints");
+            collectedHoney = PlayerPrefs.GetInt("HoneyAmount");
+            maxHoneyAmount = PlayerPrefs.GetInt("MaxHoneyAmount");
+            swordAura = PlayerPrefs.GetInt("SworsEffect");
+        }
+                                                   
+                                                   
+    }
     public void SaveGame()
     {
         PlayerPrefs.SetFloat("posX",gameObject.transform.position.x);
         PlayerPrefs.SetFloat("posY",gameObject.transform.position.y);
         PlayerPrefs.SetFloat("posZ",gameObject.transform.position.z);
-        PlayerPrefs.SetInt("LevelNumber",LevelNum);
+        PlayerPrefs.SetInt("ltl",SceneManager.GetActiveScene().buildIndex);
         PlayerPrefs.SetInt("HoneyAmount",collectedHoney);
         PlayerPrefs.SetInt("MaxHoneyAmount",maxHoneyAmount);
+        PlayerPrefs.SetInt("HealthPoints",maxHp);
         PlayerPrefs.SetInt("SworsEffect", swordAura);
         for (int i = 0; i < shopList.Count; i++)
         {
